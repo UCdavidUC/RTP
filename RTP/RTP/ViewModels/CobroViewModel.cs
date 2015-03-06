a9592cf60e2f1dcde2e06422286ed4f173ed1d20
David Sanchez <divad.dad@gmail.com>
2015-03-06 05:56:54 -0600

    Views

---
+++ RTP/RTP/ViewModels/CobroViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cirrious.MvvmCross.ViewModels;
+using System.Windows.Input;
+
+namespace RTP.ViewModels
+{
+    class CobroViewModel : MvxViewModel
+    {
+        private int numeroPasajeros = 1;
+
+        public int NumeroPasajeros
+        {
+            get { return numeroPasajeros; }
+            set { numeroPasajeros = value; RaisePropertyChanged(() => NumeroPasajeros); }
+        }
+
+        public ICommand plusAction
+        {
+            get { return new MvxCommand(() => 
+            {
+                NumeroPasajeros++;
+            }); }
+        }
+
+        public ICommand minusAction
+        {
+            get
+            {
+                return new MvxCommand(() =>
+                {
+                    NumeroPasajeros--;
+                });
+            }
+        }
+
+        public ICommand Cobro
+        {
+            get { return new MvxCommand(() => ShowViewModel<QRViewModel>()); }
+        }
+    }
+}
---
+++ RTP/RTP/ViewModels/QRViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Cirrious.MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTP.ViewModels
+{
+    class QRViewModel : MvxViewModel
+    {
+
+    }
+}
