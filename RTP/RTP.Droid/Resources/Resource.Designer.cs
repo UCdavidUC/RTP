530e800b5f60b0ae137ee1330d9a97b05a86bde2
David Sanchez <divad.dad@gmail.com>
2015-03-06 00:02:59 -0600

    Submit

--- RTP/RTP.Droid/Resources/Resource.Designer.cs
+++ RTP/RTP.Droid/Resources/Resource.Designer.cs
@@ -26,29 +26,149 @@ namespace RTP.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Acr.MvvmCross.Plugins.BarCodeScanner.Droid.Resource.Id.contentFrame = global::RTP.Droid.Resource.Id.contentFrame;
+			global::Acr.MvvmCross.Plugins.BarCodeScanner.Droid.Resource.Layout.zxingscanneractivitylayout = global::RTP.Droid.Resource.Layout.zxingscanneractivitylayout;
+			global::Acr.MvvmCross.Plugins.BarCodeScanner.Droid.Resource.Layout.zxingscannerfragmentlayout = global::RTP.Droid.Resource.Layout.zxingscannerfragmentlayout;
+			global::Acr.MvvmCross.Plugins.BarCodeScanner.Droid.Resource.String.ApplicationName = global::RTP.Droid.Resource.String.ApplicationName;
+			global::Acr.MvvmCross.Plugins.BarCodeScanner.Droid.Resource.String.Hello = global::RTP.Droid.Resource.String.Hello;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahBarColor = global::RTP.Droid.Resource.Attribute.ahBarColor;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahBarLength = global::RTP.Droid.Resource.Attribute.ahBarLength;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahBarWidth = global::RTP.Droid.Resource.Attribute.ahBarWidth;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahCircleColor = global::RTP.Droid.Resource.Attribute.ahCircleColor;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahDelayMillis = global::RTP.Droid.Resource.Attribute.ahDelayMillis;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahRadius = global::RTP.Droid.Resource.Attribute.ahRadius;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahRimColor = global::RTP.Droid.Resource.Attribute.ahRimColor;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahRimWidth = global::RTP.Droid.Resource.Attribute.ahRimWidth;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahSpinSpeed = global::RTP.Droid.Resource.Attribute.ahSpinSpeed;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahText = global::RTP.Droid.Resource.Attribute.ahText;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahTextColor = global::RTP.Droid.Resource.Attribute.ahTextColor;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Attribute.ahTextSize = global::RTP.Droid.Resource.Attribute.ahTextSize;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Drawable.ic_errorstatus = global::RTP.Droid.Resource.Drawable.ic_errorstatus;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Drawable.ic_successstatus = global::RTP.Droid.Resource.Drawable.ic_successstatus;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Drawable.roundedbg = global::RTP.Droid.Resource.Drawable.roundedbg;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Drawable.roundedbgdark = global::RTP.Droid.Resource.Drawable.roundedbgdark;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Id.loadingImage = global::RTP.Droid.Resource.Id.loadingImage;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Id.loadingProgressBar = global::RTP.Droid.Resource.Id.loadingProgressBar;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Id.loadingProgressWheel = global::RTP.Droid.Resource.Id.loadingProgressWheel;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Id.textViewStatus = global::RTP.Droid.Resource.Id.textViewStatus;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Layout.loading = global::RTP.Droid.Resource.Layout.loading;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Layout.loadingimage = global::RTP.Droid.Resource.Layout.loadingimage;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Layout.loadingprogress = global::RTP.Droid.Resource.Layout.loadingprogress;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.String.ApplicationName = global::RTP.Droid.Resource.String.ApplicationName;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.String.Hello = global::RTP.Droid.Resource.String.Hello;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.String.library_name = global::RTP.Droid.Resource.String.library_name;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel = global::RTP.Droid.Resource.Styleable.ProgressWheel;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahBarColor = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahBarColor;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahBarLength = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahBarLength;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahBarWidth = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahBarWidth;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahCircleColor = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahCircleColor;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahDelayMillis = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahDelayMillis;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahRadius = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahRadius;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahRimColor = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahRimColor;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahRimWidth = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahRimWidth;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahSpinSpeed = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahSpinSpeed;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahText = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahText;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahTextColor = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahTextColor;
+			global::Acr.MvvmCross.Plugins.UserDialogs.Droid.Resource.Styleable.ProgressWheel_ahTextSize = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahTextSize;
+			global::AndroidHUD.Resource.Attribute.ahBarColor = global::RTP.Droid.Resource.Attribute.ahBarColor;
+			global::AndroidHUD.Resource.Attribute.ahBarLength = global::RTP.Droid.Resource.Attribute.ahBarLength;
+			global::AndroidHUD.Resource.Attribute.ahBarWidth = global::RTP.Droid.Resource.Attribute.ahBarWidth;
+			global::AndroidHUD.Resource.Attribute.ahCircleColor = global::RTP.Droid.Resource.Attribute.ahCircleColor;
+			global::AndroidHUD.Resource.Attribute.ahDelayMillis = global::RTP.Droid.Resource.Attribute.ahDelayMillis;
+			global::AndroidHUD.Resource.Attribute.ahRadius = global::RTP.Droid.Resource.Attribute.ahRadius;
+			global::AndroidHUD.Resource.Attribute.ahRimColor = global::RTP.Droid.Resource.Attribute.ahRimColor;
+			global::AndroidHUD.Resource.Attribute.ahRimWidth = global::RTP.Droid.Resource.Attribute.ahRimWidth;
+			global::AndroidHUD.Resource.Attribute.ahSpinSpeed = global::RTP.Droid.Resource.Attribute.ahSpinSpeed;
+			global::AndroidHUD.Resource.Attribute.ahText = global::RTP.Droid.Resource.Attribute.ahText;
+			global::AndroidHUD.Resource.Attribute.ahTextColor = global::RTP.Droid.Resource.Attribute.ahTextColor;
+			global::AndroidHUD.Resource.Attribute.ahTextSize = global::RTP.Droid.Resource.Attribute.ahTextSize;
+			global::AndroidHUD.Resource.Drawable.ic_errorstatus = global::RTP.Droid.Resource.Drawable.ic_errorstatus;
+			global::AndroidHUD.Resource.Drawable.ic_successstatus = global::RTP.Droid.Resource.Drawable.ic_successstatus;
+			global::AndroidHUD.Resource.Drawable.roundedbg = global::RTP.Droid.Resource.Drawable.roundedbg;
+			global::AndroidHUD.Resource.Drawable.roundedbgdark = global::RTP.Droid.Resource.Drawable.roundedbgdark;
+			global::AndroidHUD.Resource.Id.loadingImage = global::RTP.Droid.Resource.Id.loadingImage;
+			global::AndroidHUD.Resource.Id.loadingProgressBar = global::RTP.Droid.Resource.Id.loadingProgressBar;
+			global::AndroidHUD.Resource.Id.loadingProgressWheel = global::RTP.Droid.Resource.Id.loadingProgressWheel;
+			global::AndroidHUD.Resource.Id.textViewStatus = global::RTP.Droid.Resource.Id.textViewStatus;
+			global::AndroidHUD.Resource.Layout.loading = global::RTP.Droid.Resource.Layout.loading;
+			global::AndroidHUD.Resource.Layout.loadingimage = global::RTP.Droid.Resource.Layout.loadingimage;
+			global::AndroidHUD.Resource.Layout.loadingprogress = global::RTP.Droid.Resource.Layout.loadingprogress;
+			global::AndroidHUD.Resource.String.library_name = global::RTP.Droid.Resource.String.library_name;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel = global::RTP.Droid.Resource.Styleable.ProgressWheel;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahBarColor = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahBarColor;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahBarLength = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahBarLength;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahBarWidth = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahBarWidth;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahCircleColor = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahCircleColor;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahDelayMillis = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahDelayMillis;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahRadius = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahRadius;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahRimColor = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahRimColor;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahRimWidth = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahRimWidth;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahSpinSpeed = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahSpinSpeed;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahText = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahText;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahTextColor = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahTextColor;
+			global::AndroidHUD.Resource.Styleable.ProgressWheel_ahTextSize = global::RTP.Droid.Resource.Styleable.ProgressWheel_ahTextSize;
 			global::Splat.Resource.String.library_name = global::RTP.Droid.Resource.String.library_name;
+			global::ZXing.Mobile.Resource.Id.contentFrame = global::RTP.Droid.Resource.Id.contentFrame;
+			global::ZXing.Mobile.Resource.Layout.zxingscanneractivitylayout = global::RTP.Droid.Resource.Layout.zxingscanneractivitylayout;
+			global::ZXing.Mobile.Resource.Layout.zxingscannerfragmentlayout = global::RTP.Droid.Resource.Layout.zxingscannerfragmentlayout;
 		}
 		
 		public partial class Attribute
 		{
 			
-			// aapt resource value: 0x7f010000
-			public const int MvxBind = 2130771968;
+			// aapt resource value: 0x7f01000c
+			public const int MvxBind = 2130771980;
 			
-			// aapt resource value: 0x7f010004
-			public const int MvxDropDownItemTemplate = 2130771972;
+			// aapt resource value: 0x7f010010
+			public const int MvxDropDownItemTemplate = 2130771984;
+			
+			// aapt resource value: 0x7f01000f
+			public const int MvxItemTemplate = 2130771983;
+			
+			// aapt resource value: 0x7f01000d
+			public const int MvxLang = 2130771981;
+			
+			// aapt resource value: 0x7f010011
+			public const int MvxSource = 2130771985;
+			
+			// aapt resource value: 0x7f01000e
+			public const int MvxTemplate = 2130771982;
 			
 			// aapt resource value: 0x7f010003
-			public const int MvxItemTemplate = 2130771971;
+			public const int ahBarColor = 2130771971;
 			
-			// aapt resource value: 0x7f010001
-			public const int MvxLang = 2130771969;
+			// aapt resource value: 0x7f01000b
+			public const int ahBarLength = 2130771979;
+			
+			// aapt resource value: 0x7f01000a
+			public const int ahBarWidth = 2130771978;
+			
+			// aapt resource value: 0x7f010008
+			public const int ahCircleColor = 2130771976;
+			
+			// aapt resource value: 0x7f010007
+			public const int ahDelayMillis = 2130771975;
+			
+			// aapt resource value: 0x7f010009
+			public const int ahRadius = 2130771977;
+			
+			// aapt resource value: 0x7f010004
+			public const int ahRimColor = 2130771972;
 			
 			// aapt resource value: 0x7f010005
-			public const int MvxSource = 2130771973;
+			public const int ahRimWidth = 2130771973;
+			
+			// aapt resource value: 0x7f010006
+			public const int ahSpinSpeed = 2130771974;
+			
+			// aapt resource value: 0x7f010000
+			public const int ahText = 2130771968;
+			
+			// aapt resource value: 0x7f010001
+			public const int ahTextColor = 2130771969;
 			
 			// aapt resource value: 0x7f010002
-			public const int MvxTemplate = 2130771970;
+			public const int ahTextSize = 2130771970;
 			
 			static Attribute()
 			{
@@ -64,13 +184,25 @@ namespace RTP.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int icon = 2130837504;
+			public const int ic_errorstatus = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int monoandroidsplash = 2130837505;
+			public const int ic_successstatus = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int splash = 2130837506;
+			public const int icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int monoandroidsplash = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int roundedbg = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int roundedbgdark = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int splash = 2130837510;
 			
 			static Drawable()
 			{
@@ -87,6 +219,21 @@ namespace RTP.Droid
 			
 			// aapt resource value: 0x7f050000
 			public const int MvxBindingTagUnique = 2131034112;
+			
+			// aapt resource value: 0x7f050005
+			public const int contentFrame = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int loadingImage = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int loadingProgressBar = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int loadingProgressWheel = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int textViewStatus = 2131034114;
 			
 			static Id()
 			{
@@ -105,7 +252,22 @@ namespace RTP.Droid
 			public const int InicialView = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int SplashScreen = 2130903041;
+			public const int loading = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int loadingimage = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int loadingprogress = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int SplashScreen = 2130903044;
+			
+			// aapt resource value: 0x7f030005
+			public const int zxingscanneractivitylayout = 2130903045;
+			
+			// aapt resource value: 0x7f030006
+			public const int zxingscannerfragmentlayout = 2130903046;
 			
 			static Layout()
 			{
@@ -119,6 +281,12 @@ namespace RTP.Droid
 		
 		public partial class String
 		{
+			
+			// aapt resource value: 0x7f040002
+			public const int ApplicationName = 2130968578;
+			
+			// aapt resource value: 0x7f040001
+			public const int Hello = 2130968577;
 			
 			// aapt resource value: 0x7f040000
 			public const int library_name = 2130968576;
@@ -156,8 +324,8 @@ namespace RTP.Droid
 		{
 			
 			public static int[] MvxBinding = new int[] {
-					2130771968,
-					2130771969};
+					2130771980,
+					2130771981};
 			
 			// aapt resource value: 0
 			public const int MvxBinding_MvxBind = 0;
@@ -166,26 +334,76 @@ namespace RTP.Droid
 			public const int MvxBinding_MvxLang = 1;
 			
 			public static int[] MvxControl = new int[] {
-					2130771970};
+					2130771982};
 			
 			// aapt resource value: 0
 			public const int MvxControl_MvxTemplate = 0;
 			
 			public static int[] MvxImageView = new int[] {
-					2130771973};
+					2130771985};
 			
 			// aapt resource value: 0
 			public const int MvxImageView_MvxSource = 0;
 			
 			public static int[] MvxListView = new int[] {
-					2130771971,
-					2130771972};
+					2130771983,
+					2130771984};
 			
 			// aapt resource value: 1
 			public const int MvxListView_MvxDropDownItemTemplate = 1;
 			
 			// aapt resource value: 0
 			public const int MvxListView_MvxItemTemplate = 0;
+			
+			public static int[] ProgressWheel = new int[] {
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971,
+					2130771972,
+					2130771973,
+					2130771974,
+					2130771975,
+					2130771976,
+					2130771977,
+					2130771978,
+					2130771979};
+			
+			// aapt resource value: 3
+			public const int ProgressWheel_ahBarColor = 3;
+			
+			// aapt resource value: 11
+			public const int ProgressWheel_ahBarLength = 11;
+			
+			// aapt resource value: 10
+			public const int ProgressWheel_ahBarWidth = 10;
+			
+			// aapt resource value: 8
+			public const int ProgressWheel_ahCircleColor = 8;
+			
+			// aapt resource value: 7
+			public const int ProgressWheel_ahDelayMillis = 7;
+			
+			// aapt resource value: 9
+			public const int ProgressWheel_ahRadius = 9;
+			
+			// aapt resource value: 4
+			public const int ProgressWheel_ahRimColor = 4;
+			
+			// aapt resource value: 5
+			public const int ProgressWheel_ahRimWidth = 5;
+			
+			// aapt resource value: 6
+			public const int ProgressWheel_ahSpinSpeed = 6;
+			
+			// aapt resource value: 0
+			public const int ProgressWheel_ahText = 0;
+			
+			// aapt resource value: 1
+			public const int ProgressWheel_ahTextColor = 1;
+			
+			// aapt resource value: 2
+			public const int ProgressWheel_ahTextSize = 2;
 			
 			static Styleable()
 			{
