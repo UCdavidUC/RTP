bd95c5a073f36cc39ab725aee2e014c5876401e8
David Sanchez <divad.dad@gmail.com>
2015-03-05 22:26:32 -0600

    All View Submit
    All Views

---
+++ RTP/RTP.WinPhone/Views/ErrorLoginView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Cirrious.MvvmCross.WindowsPhone.Views;
+
+namespace RTP.WinPhone.Views
+{
+    public partial class ErrorLoginView : MvxPhonePage
+    {
+        public ErrorLoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
