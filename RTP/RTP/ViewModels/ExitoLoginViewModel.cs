399ed5ab0941e8ff8b9889035dc2a34ddb103294
Arturo Torres Sánchez <r2d2.art2005@gmail.com>
2015-03-06 05:44:17 -0600

    HistorialViewModel

--- RTP/RTP/ViewModels/ExitoLoginViewModel.cs
+++ RTP/RTP/ViewModels/ExitoLoginViewModel.cs
@@ -3,10 +3,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace RTP.ViewModels
 {
 	public class ExitoLoginViewModel : MvxViewModel
 	{
+		public ICommand Cobrar
+		{
+			get
+			{
+				return null;
+				//return new MvxCommand(() => ShowViewModel<CobroViewModel>());
+			}
+		}
+
+		public ICommand Historial
+		{
+			get
+			{
+				return new MvxCommand(() => ShowViewModel<HistorialViewModel>());
+			}
+		}
 	}
 }
