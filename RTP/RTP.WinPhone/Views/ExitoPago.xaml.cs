abe05d729140dd4c259373cb78d5ef00c597530d
David Sanchez <divad.dad@gmail.com>
2015-03-05 18:34:27 -0600

    Vistas
    Vistas del Pasajero

---
+++ RTP/RTP.WinPhone/Views/ExitoPago.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace RTP.WinPhone.Views
+{
+    public partial class ExitoPago : PhoneApplicationPage
+    {
+        public ExitoPago()
+        {
+            InitializeComponent();
+        }
+    }
+}
